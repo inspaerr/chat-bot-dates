ae13da469d867370fc37355ba4fa532d38e287c2
Sofya <oldbox221b@gmail.com>
2023-03-13 17:38:16 +0300

    added defining users funcs

--- main/mongo.py
+++ main/mongo.py
@@ -49,3 +49,53 @@ class MongoManager:
             return None
         else:
             return request[0]
+    async def user_as_info(self, tgid):
+        user = await self.get_user(tgid)
+        return f"Информация о твоём(-ей) собеседнике(-це)\n" \
+               f"Имя: {user['name']}\n" \
+               f"Возраст: {user['age']}\n" \
+               f"О себе: {user['bio']}\n"
+
+    async def get_chat(self, tgid):
+        db: AsyncIOMotorDatabase = self.client[MasterSettings.MONGO_DB]
+        collection: AsyncIOMotorCollection = db[self.chat_col]
+        return await collection.find_one({'tgid': tgid})
+
+    async def user_registered(self, tgid):
+        db: AsyncIOMotorDatabase = self.client[MasterSettings.MONGO_DB]
+        collection: AsyncIOMotorCollection = db[self.users_col]
+        return await collection.find_one({"tgid": tgid})
+
+    async def register_user(self, tgid):
+        db: AsyncIOMotorDatabase = self.client[MasterSettings.MONGO_DB]
+        collection: AsyncIOMotorCollection = db[self.users_col]
+        if not await self.user_registered(tgid):
+            await collection.insert_one(
+                {
+                    "tgid": tgid,
+                    "name": None,
+                    "age": None,
+                    "bio": None,
+                    "status": 0
+                }
+            )
+
+    async def get_user(self, tgid):
+        db: AsyncIOMotorDatabase = self.client[MasterSettings.MONGO_DB]
+        collection: AsyncIOMotorCollection = db[self.users_col]
+        user = await collection.find_one({"tgid": tgid})
+        if not user:
+            await self.register_user(tgid)
+        return user
+
+    async def update_user(self, tgid, value):
+        db: AsyncIOMotorDatabase = self.client[MasterSettings.MONGO_DB]
+        collection: AsyncIOMotorCollection = db[self.users_col]
+        user = await self.get_user(tgid)
+        status = user.get('status')
+        if status == 0:
+            await collection.update_one({"tgid": tgid}, {"$set": {"name": value, "status": 1}})
+        elif status == 1:
+            await collection.update_one({"tgid": tgid}, {"$set": {"age": value, "status": 2}})
+        elif status == 2:
+            await collection.update_one({"tgid": tgid}, {"$set": {"bio": value, "status": 3}})
