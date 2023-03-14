7653eaebcf42e29306a99a670a30061e50f2c64e
Sofya <oldbox221b@gmail.com>
2023-03-14 13:48:58 +0300

    changed setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup
 
 setup(
-    name='mireabot-dates',
-    version='',
+    name='aiogram',
+    version='2.20',
     packages=['requirements.txt'],
-    url='',
-    license='',
     author='Софья',
     author_email='oldbox221b@gmail.com',
-    description=''
+)
+setup(
+    name='motor',
+    version='3.0.0',
+    packages=['requirements.txt'],
+    author='Софья',
+    author_email='oldbox221b@gmail.com',
 )
