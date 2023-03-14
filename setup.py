612713d47cf6e780f81145bdde79eafba5aa8f22
Sofya <oldbox221b@gmail.com>
2023-03-14 13:38:38 +0300

    added setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='mireabot-dates',
+    version='',
+    packages=['requirements.txt'],
+    url='',
+    license='',
+    author='Софья',
+    author_email='oldbox221b@gmail.com',
+    description=''
+)
